3111594cb60a02cea18d45b352fcad524a15d284
Bruno Nascimento <brucn89@gmail.com>
2019-07-16 22:12:58 -0300

    add

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,15 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new
+    @recipe.save
+    redirect_to recipe_path
+  end
+
 end
